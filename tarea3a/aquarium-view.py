32a787b1646881570633f041d26c4276be582892
Javier Urrutia <javier.urrutia.r@ug.uchile.cl>
2020-07-17 21:31:04 -0400

    Added 3d viewer

--- tarea3a/aquarium-view.py
+++ tarea3a/aquarium-view.py
@@ -1,7 +1,72 @@
 import argparse
 import json
-
+import matplotlib.pyplot as plt
 import numpy as np
+
+import glfw
+from OpenGL.GL import *
+import OpenGL.GL.shaders
+
+import transformations as tr
+import basic_shapes as bs
+import easy_shaders as es
+import lighting_shaders as ls
+h=0.1
+
+HELP_TEXT = """
+SPACE: toggle fill or line mode
+ENTER: toggle axis
+ARROW UP/DOWN: move camera up or down
+ARROW LEFT/RIGHT: move camera left or right
+W: zoom in
+S: Zoom out
+"""
+
+# A class to store the application control
+class Controller:
+    def __init__(self):
+        self.fillPolygon = True
+        self.showAxis = True
+        self.up = False
+        self.down = False
+        self.right = False
+        self.left = False
+        self.zoomIn = False
+        self.zoomOut = False
+
+
+# we will use the global controller as communication with the callback function
+controller = Controller()
+
+def on_key(window, key, scancode, action, mods):
+    global controller
+
+    if key == glfw.KEY_RIGHT:
+        controller.right = (action == glfw.PRESS or action == glfw.REPEAT)
+    elif key == glfw.KEY_LEFT:
+        controller.left = (action == glfw.PRESS or action == glfw.REPEAT)
+    elif key == glfw.KEY_UP:
+        controller.up = (action == glfw.PRESS or action == glfw.REPEAT)
+    elif key == glfw.KEY_DOWN:
+        controller.down = (action == glfw.PRESS or action == glfw.REPEAT)
+    elif key == glfw.KEY_W:
+        controller.zoomIn = (action == glfw.PRESS or action == glfw.REPEAT)
+    elif key == glfw.KEY_S:
+        controller.zoomOut = (action == glfw.PRESS or action == glfw.REPEAT)
+    
+    elif key == glfw.KEY_SPACE:
+        if action == glfw.PRESS:
+            controller.fillPolygon = not controller.fillPolygon
+
+    elif key == glfw.KEY_ENTER:
+        if action == glfw.PRESS:
+            controller.showAxis = not controller.showAxis
+
+    elif key == glfw.KEY_ESCAPE:
+        sys.exit()
+
+    else:
+        print('Unknown key')
 
 if __name__ == "__main__":
     # Parse arguments
@@ -22,3 +87,91 @@ if __name__ == "__main__":
     with open(args.filename, 'r') as setup_file:
         config = json.load(setup_file)
     print(config)
+
+    aq_space = np.load(config["filename"])
+
+    # Initialize glfw
+    if not glfw.init():
+        sys.exit()
+
+    width = 600
+    height = 600
+
+    window = glfw.create_window(width, height, "Forest Generator", None, None)
+
+    if not window:
+        glfw.terminate()
+        sys.exit()
+
+    print(HELP_TEXT)
+
+    glfw.make_context_current(window)
+
+    # Connecting the callback function 'on_key' to handle keyboard events
+    glfw.set_key_callback(window, on_key)
+
+    # Assembling the shader program (pipeline) with both shaders
+    mvpPipeline = es.SimpleModelViewProjectionShaderProgram()
+    phongPipeline = ls.SimplePhongShaderProgram()
+
+    # Setting up the clear screen color
+    glClearColor(0.85, 0.85, 0.85, 1.0)
+
+    # As we work in 3D, we need to check which part is in front,
+    # and which one is at the back
+    glEnable(GL_DEPTH_TEST)
+
+    gpuAxis = es.toGPUShape(bs.createAxis(7))
+
+    # Using the same view and projection matrices in the whole application
+    projection = tr.perspective(45, float(width)/float(height), 0.1, 100)
+    
+    camera_theta = 0.0
+    camera_phi = np.pi/4
+    camera_r = 3
+    ltime = 0
+    
+    while not glfw.window_should_close(window):
+        # Using GLFW to check for input events
+        glfw.poll_events()
+        time = glfw.get_time()
+        dt = time-ltime
+        ltime = time
+        camera_theta -= 2.0*dt*(controller.right - controller.left)
+        camera_phi -= 2.0*dt*(controller.up - controller.down)
+        camera_phi = np.clip(camera_phi, 0+0.00001, np.pi/2) # view matrix is NaN when phi=0
+        camera_r += 2.0*dt*(controller.zoomOut - controller.zoomIn)
+        camera_r = np.clip(camera_r, 0.7, 4)
+
+        cam_x = camera_r * np.sin(camera_phi) * np.sin(camera_theta)
+        cam_y = camera_r * np.sin(camera_phi) * np.cos(camera_theta)
+        cam_z = camera_r * np.cos(camera_phi)
+
+        viewPos = np.array([cam_x,cam_y,cam_z])
+
+        view = tr.lookAt(
+            viewPos,
+            np.array([0,0,0]),
+            np.array([0,0,1])
+        )
+
+        # Clearing the screen in both, color and depth
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # Filling or not the shapes depending on the controller state
+        if (controller.fillPolygon):
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+        else:
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+        if controller.showAxis:
+            glUseProgram(mvpPipeline.shaderProgram)
+            glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
+            glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "view"), 1, GL_TRUE, view)
+            glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "model"), 1, GL_TRUE, tr.identity())
+            mvpPipeline.drawShape(gpuAxis, GL_LINES)
+
+        # Once the render is done, buffers are swapped, showing only the complete scene.
+        glfw.swap_buffers(window)
+
+    glfw.terminate()
